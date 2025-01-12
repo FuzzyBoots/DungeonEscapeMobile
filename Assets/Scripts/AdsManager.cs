dfd99b4c3a1b51a0885ebb01f2c6b5363f2f993d
Sean Duggan <sean.duggan@gmail.com>
2025-01-12 10:28:44 -0500

    Rewarded ads ought to be in... but not working at the moment.

---
+++ Assets/Scripts/AdsManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine.Advertisements;
+using UnityEngine;
+
+public class AdsManager : MonoBehaviour, IUnityAdsInitializationListener
+{
+    [SerializeField] string _androidGameId;
+    [SerializeField] string _iOSGameId;
+    [SerializeField] bool _testMode = true;
+    private string _gameId;
+
+    void Awake()
+    {
+        InitializeAds();
+    }
+
+    public void InitializeAds()
+    {        
+#if UNITY_IOS
+            _gameId = _iOSGameId;
+#elif UNITY_ANDROID
+        _gameId = _androidGameId;
+#elif UNITY_EDITOR
+            _gameId = _androidGameId; //Only for testing the functionality in the Editor
+#endif
+        if (!Advertisement.isInitialized && Advertisement.isSupported)
+        {
+            Debug.Log("Initializing Ads");
+            Advertisement.Initialize(_gameId, _testMode, this);
+        } else
+        {
+            Debug.Log($"Initialized: {Advertisement.isInitialized} Supported: {Advertisement.isSupported}");
+        }
+    }
+
+
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete.");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+    }
+
+    public void ShowRewardedAd()
+    {        
+        Debug.Log("Showing rewarded ad");
+        Advertisement.Show("Rewarded_Android");
+    }
+}
